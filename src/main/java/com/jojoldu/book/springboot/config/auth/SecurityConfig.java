645657fa77976b0b6aeb1f4509f5d1496c751e14
pauljung <cse2012@naver.com>
2022-04-11 00:20:27 +0900

    OAuth 관련 정보 추가

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
