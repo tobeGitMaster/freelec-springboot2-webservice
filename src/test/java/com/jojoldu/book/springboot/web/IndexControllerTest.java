a9ffacfacb4a8ce25562c7c1ec229fe284753e44
pauljung <cse2012@naver.com>
2022-04-10 23:15:34 +0900

    각종 파일 추가

---
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package com.jojoldu.book.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 로딩() {
+        // when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        Assertions.assertThat(body).contains("스프링부트");
+    }
+}
